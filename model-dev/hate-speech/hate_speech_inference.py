cadc6ce97fe4a4610ca51e62866587a8bcd3854a
TYH71 <yuhoetan@gmail.com>
2022-10-01 18:35:03 +0800

    [model:add] add hate speech inference pipeline

---
+++ model-dev/hate-speech/hate_speech_inference.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn.functional as F
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+def hate_speech_inference(
+        query: str,
+        tokenizer=AutoTokenizer.from_pretrained("unitary/toxic-bert"),
+        model=AutoModelForSequenceClassification.from_pretrained("unitary/toxic-bert")
+    ):
+    id2label = model.config.id2label
+    # disables pytorch's automatic differentiation engine
+    with torch.no_grad():
+        tokens = tokenizer(query, return_tensors='pt') # convert text to token i.e. word to numbers
+        logits = model(**tokens).logits # run inference
+        proba = F.softmax(logits, dim=1) # softmax probabilities
+        return dict(
+            proba = proba,
+            id2label = id2label,
+            pred_class = id2label[proba.argmax().item()]
+        )
+
+def handler(event, context):
+    try:
+        json_res = json.dumps(hate_speech_inference(event['text']))
+        return {'statusCode': 200, 'json': json_res}
+    except Exception as e:
+        res = {"statusCode": 404, "body": e}
+        return res
