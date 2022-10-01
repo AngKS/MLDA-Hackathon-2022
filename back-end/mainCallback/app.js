5d877546d27acab8954e58158b191c7ce903609f
AKS <60446729+AngKS@users.noreply.github.com>
2022-10-01 15:34:58 +0800

    configured API Gateway WebSocket endpoint

--- back-end/mainCallback/app.js
+++ back-end/mainCallback/app.js
@@ -1,5 +1,33 @@
+const AWS = require('aws-sdk')
 
-module.exports.goodVibesMainHandler = (event, context) => {
+const db = new AWS.DynamoDB.DocumentClient()
 
-    return "Hello World!"
+const ROUTE_KEY_CONNECT = '$connect';
+const ROUTE_KEY_DISCONNECT = '$disconnect';
+
+
+
+let addConnectionId = async (ID) => {
+    const params = {
+        TableName: 'Good-Vibes-user-connections',
+        Item: {
+            connectionId: ID,
+        }
+    }
+
+    await db.put(params, (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+    }).promise()
+
+}
+
+module.exports.goodVibesMainHandler = (event, context, callback) => {
+    console.log(`[INCOMING EVENT] - ${JSON.stringify(event)}`)
+
+    const connectionId = event.requestContext.connectionId;
+    addConnectionId(connectionId).then(() => {
+        callback(null, { statusCode: 200, message: "Connected!" })
+    });
 }
