3ce43322fe49c7f32d9755ecf68f772f70947265
AKS <60446729+AngKS@users.noreply.github.com>
2022-10-03 01:41:56 +0800

    integrating sentiment analysis

--- front-end/src/components/chat/otherChat.js
+++ front-end/src/components/chat/otherChat.js
@@ -6,9 +6,59 @@ import { Avatar, Divider, Tooltip } from 'antd';
 
 function OtherChat({text}) {
 
+    const []
+
+    function emotionCategory(dict_input) {
+
+        let sigmoid_output = dict_input['outputs'][0][0]
+
+        let emotion = ''
+        if (sigmoid_output < 0.25) {
+            emotion = 'Negative'
+        } else if (sigmoid_output >= 0.25 && sigmoid_output < 0.75) {
+            emotion = 'Neutral'
+        } else {
+            emotion = 'Positive'
+        }
+
+        return emotion
+    }
+
+    dict_input = { 'outputs': [[0.00316798687]] }
+    emotionCategory(dict_input)
+
+
+    //input data here
+    data = {
+        'inputs': [text]
+    }
+
+
+
+    fetch(
+        'https://mlda-sentiment-2022.herokuapp.com/v1/models/sentiment:predict', {
+        'method': 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            emotion = emotionCategory(data)
+            console.log(emotion)
+
+            // state update code here!!!!!!!!!!!!!!!!!
+            
+        })
+
+
+
     return (
         <div>
-            <div className='w-auto p-3 message-bubble'>
+            <div className='w-auto p-3 message-bubble whitespace-normal'>
                 {text}
             </div>
         </div>
